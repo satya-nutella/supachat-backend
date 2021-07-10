e3c70d769e0acb00e892d468c0121e2e7b2e9d3e
meehawk <meehawk@yahoo.com>
2021-07-10 20:03:36 +0530

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv-defaults').config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: process.env.CLIENT_URL
+    }
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT} 🚀`);
+});
+
+io.use((socket, next) => {
+    const { uid, username } = socket.handshake.auth;
+    socket.uid = uid;
+    socket.username = username;
+    next();
+});
+
+io.on("connection", (socket) => {
+    const currentUser = socket.username;
+    const users = [];
+
+    for(const [id, socket] of io.of("/").sockets) {
+        if(socket.username !== currentUser) {
+            users.push({
+                username: socket.username,
+                uid: socket.uid
+            });
+        }
+    }
+
+    socket.emit("users", users);
+});
